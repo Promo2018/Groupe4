fb959d370ef864ca10d80eb080a16f073033f87a
Dimitri <dmitrii.trubetskoy@gmail.com>
2019-02-01 12:30:01 +0100

    Procedure stockee Calculation Prix Dossiers V2

--- BoVoyages/SQL Bovoyage/CalculPrixVoyage.sql
+++ BoVoyages/SQL Bovoyage/CalculPrixVoyage.sql
@@ -12,11 +12,16 @@ CREATE PROCEDURE CalculPrixVoyage as
 	DECLARE @Prix int = 0;
 	DECLARE @Assurance int = 0;
 	DECLARE @PrixVoyage int = 0;
+	DECLARE @NombreVoyageurs int =0;
+	DECLARE @VoyageursRemise int = 0;
+	DECLARE @PrixAvecRemise int = 0;
 
 	WHILE @ID <= 300
 		
 		BEGIN
-		
+
+		SET @NombreVoyageurs = (select distinct count(*) from Clients C, Dossiers D where D.ID = 82 AND C.DossierID = D.ID);
+
 		SET @Prix = (select Prix from Voyages V, Dossiers D where D.VoyageID=V.ID and D.ID=@ID);
 		
 		IF (select A.Cout from Assurances A, Dossiers D, JointAssurDossier J where A.ID=J.AssuranceID and D.ID=J.DossierID and D.ID=@ID) IS NOT NULL 
@@ -25,9 +30,10 @@ CREATE PROCEDURE CalculPrixVoyage as
 
 		ELSE SET @Assurance = 0;
 
-		--IF (select DateDeNaissance from Clients) > 2007 { DECLARE @Reduction = @Prix - (@Prix*0.6)}		
-
-		update Dossiers SET PrixTotal = (@Prix + @Assurance) WHERE Dossiers.ID=@ID -- - @Reduction)		-- UPDATE into a new table	(...update XXX set PrixVoyage = @PrixVoyage...)
+		SET @VoyageursRemise = (select distinct count(*) from Clients C, Dossiers D where D.ID = 82 AND C.DossierID = D.ID and (select datediff(year, DateDeNaissance, '01/02/2019')) < 12) 
+		SET @PrixAvecRemise = @Prix*0.6;
+		
+		update Dossiers SET PrixTotal = (@Prix + @Assurance)*@NombreVoyageurs + @PrixAvecRemise*@VoyageursRemise WHERE Dossiers.ID=@ID 
 			
 
 		SET @ID = @ID + 1;
@@ -38,6 +44,7 @@ Go
 execute CalculPrixVoyage
 
 select * from Dossiers
+
 
 
 --select max(ID) from Dossiers
@@ -51,4 +58,9 @@ select * from Dossiers
 select * from Voyages
 select * from Assurances
 select * from JointAssurDossier
+*/
+
+/*
+update Clients SET DateDeNaissance = '28/10/1936' where  ID=112;
+select * from Clients
 */
